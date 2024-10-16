c680994adcebeb238f31927c7cda9039746d5241
LuciaOliveraa <lucia.olivera@correo.ucu.edu.uy>
2024-10-15 22:06:06 -0300

    Creación del proyecto con sus dependencias

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <button className="button"> bulma</button>
+    </>
+  )
+}
+
+export default App
